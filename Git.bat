c6dbe5874a58257d28ec423f6729b3d6c0194e5a
Cương Huỳnh <hccuong.sdh19@hcmut.edu.vn>
2021-04-10 10:47:09 +0700

    update

---
+++ Git.bat
@@ -0,0 +1,3 @@
+start "" "https://github.com/huynhcuongsdh"
+start "" "https://www.tutorialspoint.com/artificial_intelligence_with_python/artificial_intelligence_with_python_supervised_learning_regression.htm"
+start "" "https://stanford.edu/~shervine/l/vi/teaching/cs-230/cheatsheet-recurrent-neural-networks"
